16a2e949e029f7c300553184280917b39c99327b
Nery Alexis Orellana <1005alexis@gmail.com>
2023-07-25 22:24:26 -0600

    Draft: task done

--- App.tsx
+++ App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
-import {CalculatorScreen} from './src/screens/CalculatorScreen';
+import { SafeAreaView, StyleSheet } from 'react-native';
 
-const App = () => {
-  return <CalculatorScreen />;
+import CalculatorScreen from './src/components/screens/CalculatorScreen';
+
+const App: React.FC = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <CalculatorScreen />
+    </SafeAreaView>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
