5f6b7bfaf8ccaad4c84d97660a646b45f869168e
Katia Araceli Vega Soto <katia.vega@digitalfemsa.com>
2023-07-26 11:28:28 -0700

    Changing background color

--- App.ts
+++ App.ts
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#202020',
+    backgroundColor: '#000',
     justifyContent: 'flex-end',
   },
 });
