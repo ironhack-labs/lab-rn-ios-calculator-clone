f6dbf167f3b8c3a87203007efc2e7ab866b52d1d
Geyser Ramírez <geyser.grs@gmail.com>
2023-07-26 22:23:00 -0600

    contenedor de botones de la calculadora

---
+++ src/components/CalculatorContainer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {View} from 'react-native';
+import {styles} from '../theme/CalculatorContainer.styles';
+
+const CalculatorContainer = ({children}) => {
+  return <View>{children}</View>;
+};
+
+export default CalculatorContainer;
--- src/screens/CalculatorScreen.jsx
+++ src/screens/CalculatorScreen.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import {styles} from '../theme/CalculatorScreen.styles';
+import CalculatorContainer from '../components/CalculatorContainer';
 
 const CalculatorScreen = ({number}) => {
   return (
     <View>
       <Text>{number}</Text>
+      <CalculatorContainer>
+        <Text>CalculatorContainer</Text>
+      </CalculatorContainer>
     </View>
   );
 };
