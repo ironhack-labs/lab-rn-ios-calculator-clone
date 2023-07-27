3db975545379223d41f61a8e880104d2f1370d35
Geyser Ramírez <geyser.grs@gmail.com>
2023-07-27 08:15:53 -0600

    clean

--- src/components/CalculatorContainer.tsx
+++ src/components/CalculatorContainer.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {View} from 'react-native';
-import {styles} from '../theme/CalculatorContainer.styles';
 
 const CalculatorContainer = ({children, style}) => {
   return <View style={style}>{children}</View>;
